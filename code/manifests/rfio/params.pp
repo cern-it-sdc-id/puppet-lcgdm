e4954238a3141399306b323e737061a3ce958326
Martin Hellmich <mhellmic@gmail.com>
2013-11-04 18:14:11 +0100

    fix whitespace

--- code/manifests/rfio/params.pp
+++ code/manifests/rfio/params.pp
@@ -1,13 +1,13 @@
 class lcgdm::rfio::params (
 ) inherits lcgdm::base::params {
 
-  $active		= "yes"
-  $ulimitn		= 4096
-  $coredump		= "no"
-  $numthreads		= 20
-  $portrange		= "20000 25000"
-  $startoptions		= "-sl"
-  $nshost         = $fqdn
-  $dpmhost        = $nshost
+  $active       = "yes"
+  $ulimitn      = 4096
+  $coredump     = "no"
+  $numthreads   = 20
+  $portrange    = "20000 25000"
+  $startoptions = "-sl"
+  $nshost       = $fqdn
+  $dpmhost      = $nshost
 
 }
