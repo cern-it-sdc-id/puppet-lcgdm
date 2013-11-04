7e37cee31411d5c722db0d45e68f0f63b38d6365
Martin Hellmich <mhellmic@gmail.com>
2013-11-04 17:26:51 +0100

    execute only if similar line does not exist
    now augeas should run only on changes,
    not everytime

--- code/manifests/shift/value.pp
+++ code/manifests/shift/value.pp
@@ -1,4 +1,4 @@
-define lcgdm::shift::value($component, $type, $value) {
+define lcgdm::shift::value($component=$title, $type, $value) {
 
     augeas { "shiftvalue_$component-$type-$value":
       context => "/files/etc/shift.conf",
@@ -8,6 +8,7 @@ define lcgdm::shift::value($component, $type, $value) {
         "set name[last()]/type $type",
         "set name[last()]/value[0] '$value'",
       ],
+      onlyif  => "match name[.='$component'][type='$type'] size == 0",
       require => [ File["/usr/share/augeas/lenses/dist/shift.aug"], File["/etc/shift.conf"], ]
     }
 
