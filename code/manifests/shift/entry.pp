ca0fb7040b0b5829a09b90ed6f5d377f80af7838
Martin Hellmich <mhellmic@gmail.com>
2013-11-05 18:26:02 +0100

    add onlyif param to shift::entry.pp

--- code/manifests/shift/entry.pp
+++ code/manifests/shift/entry.pp
@@ -1,13 +1,14 @@
 define lcgdm::shift::entry($component, $type) {
 
-    augeas { "shiftentry_${component}-${type}":
-      context => "/files/etc/shift.conf",
-      changes => [
-        "rm name[.='$component'][type='$type']",
-        "set name[last()+1] $component",
-        "set name[last()]/type $type",
+  augeas { "shiftentry_${component}-${type}":
+    context => "/files/etc/shift.conf",
+    changes => [
+      "rm name[.='$component'][type='$type']",
+      "set name[last()+1] $component",
+      "set name[last()]/type $type",
     ],
-      require => [ File["/usr/share/augeas/lenses/dist/shift.aug"], File["/etc/shift.conf"], ],
-    }
+    onlyif  => "match name[.='$component'][type='$type'] size == 0",
+    require => [ File["/usr/share/augeas/lenses/dist/shift.aug"], File["/etc/shift.conf"], ],
+  }
 
 }
