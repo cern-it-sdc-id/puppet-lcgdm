e20c8dd0e613d709358f44c48d70e1827d873a98
Martin Hellmich <mhellmic@gmail.com>
2013-11-07 18:45:15 +0100

    fix whitespace

--- code/manifests/dpm/params.pp
+++ code/manifests/dpm/params.pp
@@ -1,18 +1,18 @@
 class lcgdm::dpm::params (
 ) inherits lcgdm::base::params {
 
-  $host			= $fqdn
-  $nshost		= $host
-  $dbflavor		= "mysql"
-  $dbhost  		= "localhost"
-  $dbmanage  		= true
-  $active		= "yes"
-  $ulimitn		= 4096
-  $coredump		= "no"
-  $numfthreads		= 20
-  $numsthreads		= 20
-  $syncget		= "yes"
-  $configfile = "/usr/etc/DPMCONFIG"
-  $reqcleantimeout = "3m"
+  $host             = $fqdn
+  $nshost           = $host
+  $dbflavor         = "mysql"
+  $dbhost           = "localhost"
+  $dbmanage         = true
+  $active           = "yes"
+  $ulimitn          = 4096
+  $coredump         = "no"
+  $numfthreads      = 20
+  $numsthreads      = 20
+  $syncget          = "yes"
+  $configfile       = "/usr/etc/DPMCONFIG"
+  $reqcleantimeout  = "3m"
 
 }
--- code/manifests/ns/params.pp
+++ code/manifests/ns/params.pp
@@ -1,17 +1,17 @@
 class lcgdm::ns::params (
 ) inherits lcgdm::base::params {
 
-  $flavor  		= "dpns"
-  $host    		= $fqdn
-  $dbflavor		= "mysql"
-  $dbhost  		= "localhost"
-  $dbmanage  		= true
-  $active		= "yes"
-  $readonly		= "no"
-  $disableautovids	= "no"
-  $ulimitn		= 4096
-  $coredump		= "no"
-  $numthreads		= 20
-  $configfile  = "/usr/etc/NSCONFIG"
+  $flavor           = "dpns"
+  $host             = $fqdn
+  $dbflavor         = "mysql"
+  $dbhost           = "localhost"
+  $dbmanage         = true
+  $active           = "yes"
+  $readonly         = "no"
+  $disableautovids  = "no"
+  $ulimitn          = 4096
+  $coredump         = "no"
+  $numthreads       = 20
+  $configfile       = "/usr/etc/NSCONFIG"
 
 }
