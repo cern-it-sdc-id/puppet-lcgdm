f9cb32b8c38f61162c5eac853c4d6d1f0953e700
Martin Hellmich <mhellmic@gmail.com>
2013-11-08 11:54:59 +0100

    LCGDM-1224: add ns.pp class for default ns configurations

---
+++ code/manifests/ns.pp
@@ -0,0 +1,20 @@
+class lcgdm::ns (
+  $flavor          = $lcgdm::ns::params::flavor,
+  $dbflavor        = $lcgdm::ns::params::dbflavor,
+  $dbuser,
+  $dbpass,
+  $dbhost          = $lcgdm::ns::params::dbhost,
+  $dbmanage        = $lcgdm::ns::params::dbmanage,
+) inherits lcgdm::ns::params {
+  class{"lcgdm::ns::config":
+    flavor   => "${flavor}",
+    dbflavor => "${dbflavor}",
+    dbuser   => "${dbuser}",
+    dbpass   => "${dbpass}",
+    dbhost   => "${dbhost}",
+    dbmanage => "${dbmanage}",
+  }
+  class{"lcgdm::ns::install":}
+  class{"lcgdm::ns::service":}
+  class{"lcgdm::ns::client":}
+}
