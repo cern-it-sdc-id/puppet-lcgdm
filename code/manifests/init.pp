512f4da5c05ad14f492549813ced78d3c5b310ea
Martin Hellmich <mhellmic@gmail.com>
2013-11-08 13:51:12 +0100

    LCGDM-1224: lcgdm convenience class
    installs and configures both dpm and dpns daemons
    can encapsulate some additional dependencies

--- code/manifests/init.pp
+++ code/manifests/init.pp
@@ -1,2 +1,31 @@
-#
+class lcgdm (
+  $dbuser,
+  $dbpass,
+  $flavor   = "dpns",
+  $dbflavor = "mysql",
+  $dbhost   = "localhost",
+) {
+  Class[Lcgdm::Ns::Service] -> Class[Lcgdm::Dpm::Service]
+  Class[Lcgdm::Ns::Service] -> Class[Lcgdm::Ns::Client]
 
+  #
+  # Nameserver client and server configuration.
+  #
+  class{"lcgdm::ns":
+    flavor   => "${flavor}",
+    dbflavor => "${dbflavor}",
+    dbuser   => "${dbuser}",
+    dbpass   => "${dbpass}",
+    dbhost   => "${dbhost}",
+  }
+
+  #
+  # DPM daemon configuration.
+  #
+  class{"lcgdm::dpm":
+    dbflavor => "${dbflavor}",
+    dbuser   => "${dbuser}",
+    dbpass   => "${dbpass}",
+    dbhost   => "${dbhost}",
+  }
+}
