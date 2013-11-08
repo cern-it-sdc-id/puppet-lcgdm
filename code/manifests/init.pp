7087e29a5d27fbfbb614bd4e092ab4a586448e24
Martin Hellmich <mhellmic@gmail.com>
2013-11-08 17:06:31 +0100

    LCGDM-1224: add folder creation to init
    init.pp now also creates the typical DPM directories:
    /dpm/<domain> and
    /dpm/<domain>/home/<vo>

--- code/manifests/init.pp
+++ code/manifests/init.pp
@@ -1,12 +1,16 @@
 class lcgdm (
   $dbuser,
   $dbpass,
+  $domain,
+  $volist,
   $flavor   = "dpns",
   $dbflavor = "mysql",
   $dbhost   = "localhost",
 ) {
   Class[Lcgdm::Ns::Service] -> Class[Lcgdm::Dpm::Service]
   Class[Lcgdm::Ns::Service] -> Class[Lcgdm::Ns::Client]
+  Class[Lcgdm::Dpm::Service] -> Lcgdm::Ns::Domain <| |>
+  Lcgdm::Ns::Domain <| |> -> Lcgdm::Ns::Vo <| |>
 
   #
   # Nameserver client and server configuration.
@@ -27,5 +31,13 @@ class lcgdm (
     dbuser   => "${dbuser}",
     dbpass   => "${dbpass}",
     dbhost   => "${dbhost}",
+  }
+
+  #
+  # Create path for domain and VOs to be enabled.
+  #
+  lcgdm::ns::domain{"${domain}":}
+  lcgdm::ns::vo{$volist:
+    domain => "${domain}",
   }
 }
