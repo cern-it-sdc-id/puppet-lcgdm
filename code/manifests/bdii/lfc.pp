05441412c69c5c12abf9033dc86fbc7ce6db2433
Ignacio Reguero <Ignacio.Reguero@cern.ch>
2013-11-28 14:17:34 +0100

    fix quotes in template for LFC information provider

--- code/manifests/bdii/lfc.pp
+++ code/manifests/bdii/lfc.pp
@@ -11,7 +11,7 @@ class lcgdm::bdii::lfc (
     group => ldap,
     mode  => 755,
     content => inline_template("#! /bin/sh
-/usr/libexec/lcg-info-provider-lfc <% if @glue2 %>--glue2 <% end %>--site <%= @sitename %><% if not @localvos.empty? %> --local \"<% localvos.each do |localvo| %> <%= localvo %>\"<% end %><% end %><% if not @centralvos.empty? %> --central \"<% centralvos.each do |centralvo| %> <%= centralvo %>\"<% end %><% end %><% if @lfcalias %> --alias <%= @lfcalias %><% end %>
+/usr/libexec/lcg-info-provider-lfc <% if @glue2 %>--glue2 <% end %>--site <%= @sitename %><% if not @localvos.empty? %> --local \"<% localvos.each do |localvo| %> <%= localvo %><% end %>\"<% end %><% if not @centralvos.empty? %> --central \"<% centralvos.each do |centralvo| %> <%= centralvo %><% end %>\"<% end %><% if @lfcalias %> --alias <%= @lfcalias %><% end %>
     ")
   }
 }
