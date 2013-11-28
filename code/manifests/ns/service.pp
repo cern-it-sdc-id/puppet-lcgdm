aefcf9c8861f47cae3d50befcc1ca0185986a6b5
Andrea Manzi <andrea.manzi@cern.ch>
2013-11-28 13:13:04 +0100

    added check for dbmanage and mysql dbflavor

--- code/manifests/ns/service.pp
+++ code/manifests/ns/service.pp
@@ -1,9 +1,15 @@
 class lcgdm::ns::service (
+$dbmanage        = $lcgdm::ns::params::dbmanage,
+$dbflavor        = $lcgdm::ns::params::dbflavor
 ) inherits lcgdm::ns::params {
 
    Class[Lcgdm::Ns::Install] -> Class[Lcgdm::Ns::Service]
-   Class[Lcgdm::Ns::Mysql] ~> Class[Lcgdm::Ns::Service]
 
+   if $dbflavor == "mysql" && $dbmanage == 'true' {
+
+   	Class[Lcgdm::Ns::Mysql] ~> Class[Lcgdm::Ns::Service]
+
+   }
 
    service { "${lcgdm::ns::config::daemon}":
      enable     => true,
