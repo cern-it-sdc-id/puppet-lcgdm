8e896d12cb55a59eae0e4f0a0ea832e7d951d96a
Martin Hellmich <mhellmic@gmail.com>
2014-04-04 16:22:20 +0200

    add dependencies between exec calls
    in ns/domain and ns/vo we have multiple exec calls
    which must be in order.
    
    move the acl checking of $domain/home to ns/vo,
    because it's not created in $domain
    
    make all exec calls unique by adding $name to their names.

--- code/manifests/ns/domain.pp
+++ code/manifests/ns/domain.pp
@@ -15,26 +15,19 @@ define lcgdm::ns::domain() {
       command     => "${lcgdm::ns::config::flavor}-mkdir -p $domainpath",
       unless      => "${lcgdm::ns::config::flavor}-ls $domainpath",
     }
-
-     exec {"ns_basepath_setacl":
-       path        => "/usr/bin:/usr/sbin:/bin",
-       environment => ["CSEC_MECH=ID", "$envhost=${lcgdm::ns::config::host}", "$envtimeout=1", "$envretry=1", "$envretryint=1"],
-       command     => "${lcgdm::ns::config::flavor}-setacl -m d:u::7,d:g::7,d:o:5 /${lcgdm::ns::config::basepath}",
-       unless      => "${lcgdm::ns::config::flavor}-getacl -d /${lcgdm::ns::config::basepath} |  grep user::",
-     }
-
-     exec {"ns_basepath_setacl-domain":
-       path        => "/usr/bin:/usr/sbin:/bin",
-       environment => ["CSEC_MECH=ID", "$envhost=${lcgdm::ns::config::host}", "$envtimeout=1", "$envretry=1", "$envretryint=1"],
-       command     => "${lcgdm::ns::config::flavor}-setacl -m d:u::7,d:g::7,d:o:5 $domainpath",
-       unless      => "${lcgdm::ns::config::flavor}-getacl -d $domainpath |  grep user::",
-     }
-
-     exec {"ns_basepath_setacl-dpnsbasedir":
-       path        => "/usr/bin:/usr/sbin:/bin",
-       environment => ["CSEC_MECH=ID", "$envhost=${lcgdm::ns::config::host}", "$envtimeout=1", "$envretry=1", "$envretryint=1"],
-       command     => "${lcgdm::ns::config::flavor}-setacl -m d:u::7,d:g::7,d:o:5 $domainpath/home",
-       unless      => "${lcgdm::ns::config::flavor}-getacl -d $domainpath/home |  grep user::",
-     }
+    ->
+    exec {"ns_basepath_setacl-$name":
+      path        => "/usr/bin:/usr/sbin:/bin",
+      environment => ["CSEC_MECH=ID", "$envhost=${lcgdm::ns::config::host}", "$envtimeout=1", "$envretry=1", "$envretryint=1"],
+      command     => "${lcgdm::ns::config::flavor}-setacl -m d:u::7,d:g::7,d:o:5 /${lcgdm::ns::config::basepath}",
+      unless      => "${lcgdm::ns::config::flavor}-getacl -d /${lcgdm::ns::config::basepath} |  grep user::",
+    }
+    ->
+    exec {"ns_basepath_setacl-domain-$name":
+      path        => "/usr/bin:/usr/sbin:/bin",
+      environment => ["CSEC_MECH=ID", "$envhost=${lcgdm::ns::config::host}", "$envtimeout=1", "$envretry=1", "$envretryint=1"],
+      command     => "${lcgdm::ns::config::flavor}-setacl -m d:u::7,d:g::7,d:o:5 $domainpath",
+      unless      => "${lcgdm::ns::config::flavor}-getacl -d $domainpath |  grep user::",
+    }
 
 }
