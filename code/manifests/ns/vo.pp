d7ef63c5311d2eaaf68ca7eb9b22e433bcb3b700
Martin Hellmich <mhellmic@gmail.com>
2014-04-04 16:22:20 +0200

    add dependencies between exec calls
    in ns/domain and ns/vo we have multiple exec calls
    which must be in order.
    
    move the acl checking of $domain/home to ns/vo,
    because it's not created in $domain
    
    make all exec calls unique by adding $name to their names.

--- code/manifests/ns/vo.pp
+++ code/manifests/ns/vo.pp
@@ -9,7 +9,8 @@ define lcgdm::ns::vo($domain) {
     $envretry    = "${lcgdm::ns::config::envvar}_CONNRETRY"
     $envretryint = "${lcgdm::ns::config::envvar}_CONNRETRYINT"
 
-    $vopath = "/${lcgdm::ns::config::basepath}/$domain/home/$name"
+    $domainpath = "/${lcgdm::ns::config::basepath}/$domain"
+    $vopath = "/${domainpath}/home/$name"
 
     exec { "ns_vo_$domain_$name":
       path        => "/usr/bin:/usr/sbin",
@@ -17,11 +18,18 @@ define lcgdm::ns::vo($domain) {
       command     => "${lcgdm::ns::config::flavor}-mkdir -p $vopath; ${lcgdm::ns::config::flavor}-chmod 755 $vopath; ${lcgdm::ns::config::flavor}-entergrpmap --group $name; ${lcgdm::ns::config::flavor}-chown root:$name $vopath; ${lcgdm::ns::config::flavor}-chmod 775 $vopath",
       unless      => "${lcgdm::ns::config::flavor}-ls $vopath",
     }
-
-   exec {"ns_vopath_setacl-$name":
-       path        => "/usr/bin:/usr/sbin:/bin",
-       environment => ["CSEC_MECH=ID", "$envhost=${lcgdm::ns::config::host}", "$envtimeout=1", "$envretry=1", "$envretryint=1"],
-       command     => "${lcgdm::ns::config::flavor}-setacl -m d:u::7,d:g::7,d:o:5 $vopath",
-       unless      => "${lcgdm::ns::config::flavor}-getacl -d $vopath |  grep user::",
-     }
+    ->
+    exec {"ns_basepath_setacl-dpnsbasedir-$domain-$name":
+      path        => "/usr/bin:/usr/sbin:/bin",
+      environment => ["CSEC_MECH=ID", "$envhost=${lcgdm::ns::config::host}", "$envtimeout=1", "$envretry=1", "$envretryint=1"],
+      command     => "${lcgdm::ns::config::flavor}-setacl -m d:u::7,d:g::7,d:o:5 $domainpath/home",
+      unless      => "${lcgdm::ns::config::flavor}-getacl -d $domainpath/home |  grep user::",
+    }
+    ->
+    exec {"ns_vopath_setacl-$domain-$name":
+      path        => "/usr/bin:/usr/sbin:/bin",
+      environment => ["CSEC_MECH=ID", "$envhost=${lcgdm::ns::config::host}", "$envtimeout=1", "$envretry=1", "$envretryint=1"],
+      command     => "${lcgdm::ns::config::flavor}-setacl -m d:u::7,d:g::7,d:o:5 $vopath",
+      unless      => "${lcgdm::ns::config::flavor}-getacl -d $vopath |  grep user::",
+    }
 }
