05e3733dc37e04b5e2df5fad5d13cb3715bc7721
Martin Hellmich <mhellmic@gmail.com>
2014-05-23 15:54:25 +0200

    change the behaviour of adding dpmmgr uid and gid
    use ensure_resource to instantiate the base and base subclasses.
    add config params to set the dpmmgr uid and gid.

--- code/manifests/base.pp
+++ code/manifests/base.pp
@@ -1,5 +1,9 @@
-class lcgdm::base
-{
-  include('lcgdm::base::config')
-  include('lcgdm::base::install')
+class lcgdm::base (
+  $dpmuid          = $lcgdm::base::params::uid,
+  $dpmgid          = $lcgdm::base::params::gid
+) inherits lcgdm::dpm::params {
+  ensure_resource('class', 'lcgdm::base::config', { uid => $dpmuid, gid => $dpmgid, })
+  ensure_resource('class', 'lcgdm::base::install', { })
+  #include('lcgdm::base::config')
+  #include('lcgdm::base::install')
 }
