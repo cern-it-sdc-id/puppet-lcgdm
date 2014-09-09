97ad884c6f06651d39c6a7c25117633bcae030cb
andrea-manzi <andrea.manzi@gmail.com>
2014-09-09 12:25:08 +0200

    configuring cert/certkey/cert from base.pp, needed for lfc

--- code/manifests/base.pp
+++ code/manifests/base.pp
@@ -1,9 +1,15 @@
 class lcgdm::base (
   $uid          = $lcgdm::base::params::uid,
+  $cert         = $lcgdm::base::params::cert,
+  $certkey      = $lcgdm::base::params::certkey,
+  $user         = $lcgdm::base::params::user,
 ) inherits lcgdm::dpm::params {
   class{"lcgdm::base::config":
     uid => $uid,
     gid => $uid,
+    cert => $cert,
+    certkey => $certkey,
+    user => $user,
   }
   class{"lcgdm::base::install":}
 }
