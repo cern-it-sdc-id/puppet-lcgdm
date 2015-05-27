9c27b13749947281e760759b0fb86c32186730ac
andrea-manzi <andrea.manzi@gmail.com>
2015-05-27 15:51:18 +0200

    changes for /LCGDM-1735

--- manifests/bdii/dpm.pp
+++ manifests/bdii/dpm.pp
@@ -1,29 +1,45 @@
-class lcgdm::bdii::dpm ($sitename = undef, $basedir = 'home', $vos = [], $glue2 = true) {
-  file { '/var/lib/bdii/gip/provider/se-dpm':
-    owner   => root,
-    group   => root,
-    mode    => '0755',
-    content => inline_template("
+class lcgdm::bdii::dpm ($sitename = undef, $basedir = 'home', $vos = [], $glue2 = true, $hdfs = false) {
+
+  if $hdfs {
+    file { '/var/lib/bdii/gip/provider/se-dpm':
+      owner   => root,
+      group   => root,
+      mode    => '0755',
+      content => inline_template("
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java/jre/lib/amd64/server/
+export X509_USER_CERT=/var/lib/ldap/hostcert.pem
+export X509_USER_KEY=/var/lib/ldap/hostkey.pem
+dpm-listspaces --gip --protocols --basedir <%= @basedir %> --site <%= @sitename %><% if @glue2 %> --glue2<% end %> --use-dmlite-conf /etc/dmlite.conf
+      ")
+    }
+  }
+  else {  
+
+    file { '/var/lib/bdii/gip/provider/se-dpm':
+      owner   => root,
+      group   => root,
+      mode    => '0755',
+      content => inline_template("
 export X509_USER_CERT=/var/lib/ldap/hostcert.pem
 export X509_USER_KEY=/var/lib/ldap/hostkey.pem
 dpm-listspaces --gip --protocols --basedir <%= @basedir %> --site <%= @sitename %><% if @glue2 %> --glue2<% end %>
-    ")
-  }
+      ")
+    }
 
-  file { '/var/lib/bdii/gip/provider/service-srm2.2':
-    owner   => root,
-    group   => root,
-    mode    => '0755',
-    content => inline_template("
+    file { '/var/lib/bdii/gip/provider/service-srm2.2':
+      owner   => root,
+      group   => root,
+      mode    => '0755',
+      content => inline_template("
 glite-info-service /var/lib/bdii/gip/glite-info-service-srm2.2.conf <%= @sitename %> httpg://${::fqdn}:8446/srm/managerv2
-    ")
-  }
+      ")
+    }
 
-  file { '/var/lib/bdii/gip/glite-info-service-srm2.2.conf':
-    owner   => root,
-    group   => root,
-    mode    => '0755',
-    content => inline_template("
+    file { '/var/lib/bdii/gip/glite-info-service-srm2.2.conf':
+      owner   => root,
+      group   => root,
+      mode    => '0755',
+      content => inline_template("
 init = glite-info-service-dpm init v2
 service_type = SRM
 get_version = echo \$GLITE_INFO_SERVICE_VERSION
@@ -39,11 +55,14 @@ get_acbr = <% vos.sort.each do |vo| %> echo VO:<%= vo %>; <% end %>
     ")
   }
 
+  # Required for srm provider
+  package { 'glite-info-provider-service': }
+
+  }
+
   # Required for dpm-listspaces
   package { 'dpm-python': }
 
-  # Required for srm provider
-  package { 'glite-info-provider-service': }
 
   # dpm-listspaces need hostcert and key to be able to
   # connect (it is running as the ldap user)
