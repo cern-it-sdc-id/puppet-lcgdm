2815f868413a7f41d904a92cd64ad078767defbf
andrea-manzi <andrea.manzi@gmail.com>
2015-09-15 11:48:47 +0200

    added grants

---
+++ manifests/dpm/grants.pp
@@ -0,0 +1,16 @@
+define lcgdm::dpm::grant ($user, $pass) {
+    mysql_user { "${user}@${name}":
+      ensure        => present,
+      password_hash => mysql_password($pass),
+      provider      => 'mysql',
+    }
+    mysql_grant { "${user}@${name}/cns_db.*":
+      ensure     => 'present',
+      options    => ['GRANT'],
+      privileges => ['ALL'],
+      table      => 'cns_db.*',
+      user       => "${user}@${name}",
+      require    => [ Mysql_database['cns_db'], Mysql_user["${user}@${name}"] ],
+  }
+}
+
