9d2af3cbe46020a6a1e2571b2c0e59fe2ca259bb
Baptiste Grenier <bgrenier@gnubila.fr>
2016-03-08 11:15:31 +0100

    lcgdm::rfio: fix default for nshost, using another parameter as a default is not supported

--- manifests/rfio.pp
+++ manifests/rfio.pp
@@ -1,4 +1,4 @@
-class lcgdm::rfio ($dpmhost = "${::fqdn}", $nshost = "${dpmhost}",) {
+class lcgdm::rfio ($dpmhost = "${::fqdn}", $nshost = "${::fqdn}",) {
   Class[lcgdm::base::config] -> Class[lcgdm::rfio::config]
 
   class { 'lcgdm::rfio::config':
